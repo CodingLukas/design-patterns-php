fb4c14643b12c40776fcba936a6683100253ec4a
Lukas <lukasimbm@gmail.com>
2021-01-25 13:55:49 +0200

    complete Strategy pattern

---
+++ strategy/Strategy_laracasts.php
@@ -0,0 +1,48 @@
+<?php
+
+// encapsulate and make them interchangeable
+interface Logger
+{
+    public function log($data);
+}
+
+// Define a family of algorithms
+
+class LogToFile implements Logger
+{
+
+    public function log($data)
+    {
+        var_dump('Log the data to a file');
+    }
+}
+
+class LogToDatabase implements Logger
+{
+    public function log($data)
+    {
+        var_dump('Log the data to the database');
+    }
+}
+
+class LogToXWebService implements Logger
+{
+    public function log($data)
+    {
+        var_dump('Log the data to a Sass site');
+    }
+}
+
+class App
+{
+    public function log($data, Logger $logger = null)
+    {
+        $logger = $logger ?: new LogToFile;
+
+        $logger->log($data);
+    }
+}
+
+$app = new App();
+
+$app->log('Some information here', new LogToXWebService);
